1b3e5473539198de10ceff812c4843d213d857f7
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-10 22:42:34 +0300

    Implemented routes

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -36,7 +34,7 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let`s go!</h2>
 
       <form>
         <div>
