13abffe377fa0ba4c2acf28350bb021012bff467
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-11 13:58:02 +0300

    Used CSS grid

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Olga</div>;
+  return <div className="hidden text-sm font-semibold md:block">Olga</div>;
 }
 
 export default Username;
