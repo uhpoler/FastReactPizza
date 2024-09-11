767bbd76e4ade002f93c9af68d39763ec0f65386
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-11 13:30:41 +0300

    Added the box model: spacing, borders, and display

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold">Olga</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Fast React Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3">
+      <Link to="/" className="tracking-widest">
+        Fast React Co.
+      </Link>
 
       <SearchOrder />
-    </div>
+      <Username />
+    </header>
   );
 }
 
