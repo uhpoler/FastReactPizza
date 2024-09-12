377aeba430605887f419226eadfd71a3089520ed
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-12 18:43:23 +0300

    Read and updated the user state

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./src/features/user/userSlice";
+import userReducer from "./features/user/userSlice";
 
 const store = configureStore({
   reducer: {
