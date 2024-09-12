5a662d84d937eebf3d60066ec6abf664a82ba4ef
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-12 14:50:51 +0300

    Styled the menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,7 +5,7 @@ import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="dvide-stone-200 divide-y px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
